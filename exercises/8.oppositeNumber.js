94a0b3e58e13d24f7125d969e6e3456f46010185
Aitor Palleja <aitor.palleja@gmail.com>
2022-04-10 16:43:52 +0200

    oppositeNumbers function (math.abs)

---
+++ exercises/8.oppositeNumber.js
@@ -0,0 +1,35 @@
+// ****** 7 ******
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+// My solution
+
+const opposite = (number) => {
+
+    if (number > 0) {
+        return -Math.abs(number);
+    }
+    else {
+        return Math.abs(number);;
+    }
+}
+
+
+// Best Codewars solution
+
+function opposite(number) {
+    return (-number);
+}
+
+const opposite = number => -number;
+
+function opposite(number) {
+    return number * (-1);
+}
